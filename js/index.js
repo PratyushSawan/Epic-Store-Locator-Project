aab8a9c1828f2d423e074379b3578129ad28d32e
Pratyush <developer@pratyushsawan.tech>
2020-04-12 21:12:53 +0530

    Show all tge stores in the stores list based on the real world Data   Done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 
+window.onload = function () {
+    displayStores();
+}
+
 
 function initMap() {
     var india = { lat: 20.478843, lng: 79.940029 };
@@ -7,4 +11,36 @@ function initMap() {
         zoom: 8,
         mapTypeId: 'roadmap',
     });
+}
+
+//Function for showing store data
+
+function displayStores() {
+    var storesHtml = '';
+
+    for (var [index, store] of stores.entries()) {
+        var address = store['addressLines'];
+        var phone = store['phoneNumber'];
+        storesHtml += `
+      <div class="store-container">
+      <div class="store-add-info">
+          <div class="store-adress">
+             ${address[0],address[1]}
+          </div>
+          <div class="store-phone-number">
+              <i class="fas fa-phone-alt"></i>
+              ${phone}
+          </div>
+      </div>
+      <div class="store-number-container">
+          <div class="store-number">${index+1}</div>
+      </div>
+     </div>
+      <div class="hr">
+      <hr>
+       </div>
+      `
+      document.querySelector('.store-list').innerHTML = storesHtml;
+
+    }
 }
--- todo.md
+++ todo.md
@@ -1,4 +1,4 @@
-# Plan Of Action
+# Plan Of Action Day-1
 
 - Create WireFrame    DONE
 
@@ -13,3 +13,15 @@
 - Add store list Container  DONE
 
 - Add Individual  Store Container   DONE
+
+# Plan of Action Day-2
+
+- Style the Individual item in the stores list     DONE      
+
+- Show all the stores in the stores list based on real world Data   DONE
+
+- Show the marker based on reald world Store Data
+
+- Show the info window when you click on the indivudial store
+
+- Add 
